f2c4e07041cee9e3ad7960d9aab2ceea41ec3758
davidlechuga <david.lechuga@lakmisystems.com.mx>
2019-12-07 00:27:34 -0600

    ok

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,36 +1,28 @@
 export default {
-    async getTwits() {
-      // console.log('get')
-      const response = await fetch( 'https://twiitgov.mybluemix.net/search/marihuana')
-      if (!response.ok) throw new Error('Ocurrió un error al obtener los posts')
-      const jsonBody = await response.json()
-  
-      const { statuses } = jsonBody.data
+	async getTwits() {
+		// console.log('get')
+		const response = await fetch('https://twiitgov.mybluemix.net/search/marihuana');
+		if (!response.ok) throw new Error('Ocurrió un error al obtener los posts');
+		const jsonBody = await response.json();
 
-      // console.log(statuses[0].full_text);
-      // console.log(statuses[1].full_text);
-  
-      return statuses
-      
-    },
-  
-    async searchTwits() {
-      const response = await fetch('search/tweets')
-      .then((response)=>{
-        console.log(response)
-      }
-      )
-      const jsonBody = await response.json()
-  
-      const { searchtwits } = jsonBody.data
+		const { statuses } = jsonBody.data;
 
+		// console.log(statuses[0].full_text);
+		// console.log(statuses[1].full_text);n
 
-      if (!response.ok) throw new Error('Ocurrió un error al obtener los posts')
-  
-      return searchtwits
-    },
-}
-  
+		return statuses;
+	},
 
+	async searchTwits() {
+		const response = await fetch('search/tweets').then((response) => {
+			console.log(response);
+		});
+		const jsonBody = await response.json();
 
+		const { searchtwits } = jsonBody.data;
 
+		if (!response.ok) throw new Error('Ocurrió un error al obtener los posts');
+
+		return searchtwits;
+	}
+};
